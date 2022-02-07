211770216a006c51d0b8274884aca0691a2a2baa
Niveditha <niveditharc11@gmail.com>
2022-02-07 15:16:48 +0530

    first commit

---
+++ Gitt4/src/com/l/Typee.java
@@ -0,0 +1,19 @@
+package com.l;
+
+public class Typee {
+
+	public static void main(String[] args) 
+	{
+		int year =76;
+		if(year%4==0 )
+		{
+			System.out.println("leap year");
+		}
+		else
+		{
+			System.out.println("not aleap year");
+		}
+	
+	}
+	
+}
