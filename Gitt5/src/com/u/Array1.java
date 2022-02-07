fe92fd4ca2a78d0b362860f04be05c72c94ee24f
Niveditha <niveditharc11@gmail.com>
2022-02-07 18:01:48 +0530

    branch1 is commited

---
+++ Gitt5/src/com/u/Array1.java
@@ -0,0 +1,11 @@
+package com.u;
+
+public class Array1 {
+
+	public static void main(String[] args) 
+	{
+		String flowers []= {"lily","rose","lotus","tulip"};
+		System.out.println(flowers[3]);
+	}
+
+}
